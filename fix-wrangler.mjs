7304728e7fa87f4741e74bc65b03fc34d33f9fe0
James Kee <mixtapewrestling@gmail.com>
2026-06-07 00:08:31 -0400

    Fix pages_build_output_dir in wrangler patch

--- fix-wrangler.mjs
+++ fix-wrangler.mjs
@@ -8,6 +8,7 @@ delete config.kv_namespaces;
 delete config.assets;
 delete config.main;
 delete config.rules;
+delete config.pages_build_output_dir;
 
 // Add our D1 binding
 config.d1_databases = [{
@@ -15,9 +16,6 @@ config.d1_databases = [{
   database_name: 'mixtape-tickets',
   database_id: 'db53921c-0d35-4db7-a5a9-65e3137b90f4',
 }];
-
-// Set required Pages field
-config.pages_build_output_dir = 'dist';
 
 writeFileSync(path, JSON.stringify(config, null, 2));
 console.log('wrangler.json patched successfully');
