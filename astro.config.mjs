9b24e7f762aaa16c1e69c8533af8d45259e9ec72
James Kee <mixtapewrestling@gmail.com>
2026-06-07 00:39:39 -0400

    Switch back to static mode - dynamic via API only

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
-  output: 'server',
-  adapter: cloudflare({
-    imageService: 'passthrough',
-  }),
+  output: 'static',
 });
