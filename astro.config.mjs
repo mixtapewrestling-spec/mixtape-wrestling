01d9ce12affa3f0e3398329f3734226084af02f4
James Kee <mixtapewrestling@gmail.com>
2026-06-07 00:01:35 -0400

    Fix Cloudflare adapter config to remove invalid bindings

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,8 @@ import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   output: 'server',
-  adapter: cloudflare(),
+  adapter: cloudflare({
+    imageService: 'passthrough',
+    sessionKVBindingName: undefined,
+  }),
 });
